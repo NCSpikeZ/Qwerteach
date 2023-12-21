036a9c20faa6903dcc3e79baccc8718f99e86ac9
nc.coop <nicolas.coop@outlook.fr>
2023-12-21 12:03:40 +0100

    responsive 100%

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,13 +1,19 @@
-  document.addEventListener('DOMContentLoaded', function() {
-    const burgerMenu = document.querySelector('.burger-menu');
-    const navigation = document.querySelector('.navigation');
-    const navLinks = document.querySelector('.nav-links');
-    const buttons = document.querySelector('.buttons');
+document.addEventListener('DOMContentLoaded', function() {
+  const burgerMenu = document.querySelector('.burger-menu');
+  const navigation = document.querySelector('.navigation');
+  const navLinks = document.querySelector('.nav-links');
+  const buttons = document.querySelector('.buttons');
 
-    burgerMenu.addEventListener('click', function() {
-      navLinks.classList.toggle('active');
-      buttons.classList.toggle('active');
-      navigation.classList.toggle('active');
-      document.body.classList.toggle('no-scroll');
-    });
+  burgerMenu.addEventListener('click', function() {
+    if (burgerMenu.textContent === '\u2630') {
+      burgerMenu.textContent = '\u2715';
+    } else {
+      burgerMenu.textContent = '\u2630';
+    }
+    burgerMenu.classList.toggle('active');
+    navLinks.classList.toggle('active');
+    buttons.classList.toggle('active');
+    navigation.classList.toggle('active');
+    document.body.classList.toggle('no-scroll');
   });
+});
